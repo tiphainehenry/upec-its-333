71bb144bcf384d5db8635a12baf97e29e9b86bb6
th272796 <tiphaine.henry@cea.fr>
2025-01-08 09:02:40 +0100

    secondAPI

---
+++ CM1-Introduction aux APIs/mySecondAPI_with_MVC/app/views.py
@@ -0,0 +1,10 @@
+from app import app
+from flask import render_template, request, jsonify
+
+### EXO1 - simple API
+
+### EXO2 - API with simple display
+
+### EXO3 - API with parameters display 
+
+### EXO4 - API with parameters retrieved from URL 
