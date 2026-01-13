7cbfc2fcd8ce6977cc93210bd1bcc48180b14ff5
th272796 <tiphaine.henry@cea.fr>
2026-01-13 10:56:25 +0100

    added miniprojet

--- srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
+++ srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
@@ -1,10 +1,8 @@
 from app import app
 from flask import render_template, request, jsonify
 
-### EXO1 - simple API
+### EXO1 - Implement a route that returns "hello" printed in a webpage using an html template
 
-### EXO2 - API with simple display
+### EXO2 - Implement a route that returns "hello {name}" with the name declared in the route (add html template)
 
-### EXO3 - API with parameters display 
-
-### EXO4 - API with parameters retrieved from URL 
+### EXO3 - Implement a route that returns "hello {name}" with the name retrieved from url, eg retrieved from http://localhost:5000/api/salutation?nom=Alice
